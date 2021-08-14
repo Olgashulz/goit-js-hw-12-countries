8361c248c02715bd78ae19d9624a90fbbfafec77
Olgashulz <41305602+Olgashulz@users.noreply.github.com>
2021-08-14 14:57:15 +0300

    разметка

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
 import './sass/main.scss';
+const debounce = require('lodash.debounce');
+import countryCardTpl from './templates/countryCardTpl.hbs';
+
+
+const refs = {
+    inputEl: document.querySelector('#country-name-input'),
+    container: document.querySelector('.container')
+}
+
+console.log(refs.container);
+refs.inputEl.addEventListener('input', debounce(getCountry, 2000));
+
+console.log(refs.inputEl);
+
+function getCountry(event) {
+    return fetchCountry(event.target.value)
+}
+
+function fetchCountry(name) {
+    fetch(`https://restcountries.eu/rest/v2/name/${name}`)
+        .then(response => {
+            return response.json();
+        })
+        .then(country => {
+            console.log(country);
+            const markup = countryCardTpl(country);
+            console.log(markup);
+        })
+        .catch(error => {
+            console.log(error);
+        })
+}
